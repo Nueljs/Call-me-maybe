3ec3245e1a0d8a31098e924daa0a2889e59c1e46
manuel <manuelcerveralara@gmail.com>
2026-07-31 05:43:02 +0200

    a

--- src/prompt_builder.py
+++ src/prompt_builder.py
@@ -1,4 +1,8 @@
 from schemas import FunctionDefinition, TestPrompt
+import json
 
 
-def build_
+def build_context(functions: list[FunctionDefinition], user_input: str) -> str:
+    list_of_text: list[str] = [func.model_dump_json(indent=2) for func in functions]
+
+    functions_block: str = "\n\n".join(list_of_textgit )
