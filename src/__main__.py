b8ad79ebad8503ba5db6d8f2b419bb69316268b1
manuel <manuelcerveralara@gmail.com>
2026-08-01 05:47:12 +0200

    a

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,23 @@
 import argparse
+from .schemas import DataLoader, TestPrompt, FunctionDefinition
+from llm_sdk.llm_sdk import Small_LLM_Model
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Processing args')
-    parser.add_argument('--input', type=str, help='The input file')
-    parser.add_argument('--output', type=str, help='The output file')
+    parser.add_argument(
+        '--input', type=str, default="data/input/function_calling_tests.json",
+        help='The input file')
+    parser.add_argument(
+        '--output', type=str, default="data/output/function_calls.json",
+        help='The output file')
+    parser.add_argument(
+        '--functions_definition', type=str,
+        default="data/input/functions_definitions.json",
+        help='The functions file')
+    args = parser.parse_args()
+
+    prompt_loader: DataLoader = DataLoader(args.input)
+    prompts: list[TestPrompt] = prompt_loader.prompt_request()
+    function_loader: DataLoader = DataLoader(args.functions_definition)
+    functions: list[FunctionDefinition] = function_loader.function_request()
