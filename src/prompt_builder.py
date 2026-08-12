9b8ece6618977bc057cc0bfee43e49d7effc2832
Nueljs <manuelcerveralara@gmail.com>
2026-08-12 13:41:24 +0200

    a

--- src/prompt_builder.py
+++ src/prompt_builder.py
@@ -9,7 +9,11 @@ def build_context(functions: list[FunctionDefinition], user_input: str) -> str:
 
     final_prompt: str = f"""You are an expert analyst. You have access to the
     following tools:{functions_block}
-    The user said: "{user_input}"
+    The user said:
+    ---
+    {user_input}
+    ---
+
     Generate a call to the correct function in json format:\n"""
 
     return final_prompt
